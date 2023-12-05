c55eca37d528788706c898060627f555486bfbc5
JERRELL RUSS <jruss1@cnm.edu>
2023-12-04 20:59:54 -0700

    added header

--- RussInventoryCommandPattern/InventoryItems.cs
+++ RussInventoryCommandPattern/InventoryItems.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ Programmer: Jerrell Russ
+email:  jruss1@cnm.edu
+This is P6. its supposed  to show and demonstrate operation for list boxes
+ */
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
